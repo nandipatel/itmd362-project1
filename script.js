777be5cc08abaa4c9eb8bbfd213a58c1c988c6f4
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-10-06 23:44:55 -0500

    trying to add quotes to the quote

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function myFunction() {
-  document.getElementById('change').innerHTML = "Success is the sum of small efforts repeated day in and day out - Robert Collier";
+  document.getElementById('change').innerHTML = "\"Success is the sum of small efforts repeated day in and day out - Robert Collier\"";
 }
