2829901c4caf10177a20eb92582fc6bd084eaf68
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-10-28 16:31:28 -0500

    unbinded js and html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.getElementById('change').addEventListener("Inspire"), myFunction();
+document.getElementById('button1').onclick = function() {myFunction()};
 
 function myFunction() {
   document.getElementById('change').innerHTML = "\"Success is the sum of small efforts repeated day in and day out\" - Robert Collier";
