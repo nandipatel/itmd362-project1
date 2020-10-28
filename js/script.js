d89b5575a0400d9c60ce5695fd09244166a4c43a
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-10-28 16:44:30 -0500

    tried adding js validation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ document.getElementById('button1').onclick = function() {myFunction()};
 function myFunction() {
   document.getElementById('change').innerHTML = "\"Success is the sum of small efforts repeated day in and day out\" - Robert Collier";
 }
+
+function valForm() {
+  var x = document.forms["form1"]["email_id"].value;
+  if (x == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+}
