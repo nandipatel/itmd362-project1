ffc1071ee2fbda60d2650043a307004bf8be4808
nandipatel <60127512+nandipatel@users.noreply.github.com>
2020-10-28 16:21:42 -0500

    trying to unbind js to HTML and changed visibility of quote on smaller screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+document.getElementById('change').addEventListener("Inspire"), myFunction();
+
 function myFunction() {
   document.getElementById('change').innerHTML = "\"Success is the sum of small efforts repeated day in and day out\" - Robert Collier";
 }
